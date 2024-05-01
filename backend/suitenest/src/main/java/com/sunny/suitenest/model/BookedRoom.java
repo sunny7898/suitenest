26fffc50621d3d5aa52fe047c2fa0150e9e23505
Sunny Goswami <sungoswami07gmail.com>
2024-05-02 04:48:23 +0530

    entities completed - room & bookedRoom & the dto or response class created

--- backend/suitenest/src/main/java/com/sunny/suitenest/model/BookedRoom.java
+++ backend/suitenest/src/main/java/com/sunny/suitenest/model/BookedRoom.java
@@ -1,4 +1,61 @@
 package com.sunny.suitenest.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "check_in")
+    private LocalDate checkInDate;
+    @Column(name = "check_out")
+    private LocalDate checkOutDate;
+
+    @Column(name = "guest_fullname")
+    private String guestFullName;
+    @Column(name = "guest_email")
+    private String guestEmail;
+    @Column(name = "adults")
+    private int numOfAdults;
+    @Column(name = "children")
+    private int numOfChildren;
+    @Column(name = "total_guest")
+    private int totalNumOfGuest;
+
+    @Column(name = "confirmation_code")
+    private String bookingConfirmationCode;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public void calculateTotalNumberOfGuest() {
+        this.totalNumOfGuest = numOfAdults + numOfChildren;
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        this.numOfAdults = numOfAdults;
+        calculateTotalNumberOfGuest();
+    }
+
+    public void setNumOfChildren(int numOfChildren) {
+        this.numOfChildren = numOfChildren;
+        calculateTotalNumberOfGuest();
+    }
+
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
 }
