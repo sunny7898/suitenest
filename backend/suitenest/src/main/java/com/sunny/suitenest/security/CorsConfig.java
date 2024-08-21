c9572c132a932776bc87e48101664261bd150f38
Sunny Goswami <sungoswami07gmail.com>
2024-08-22 03:20:58 +0530

    Deployment - Phase 1 completed

--- backend/suitenest/src/main/java/com/sunny/suitenest/security/CorsConfig.java
+++ backend/suitenest/src/main/java/com/sunny/suitenest/security/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.sunny.suitenest.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,12 +20,15 @@ public class CorsConfig {
     private static final Long MAX_AGE = 3600L;
     private static final int CORS_FILTER_ORDER = -102;
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Bean
     public FilterRegistrationBean filterRegistrationBean() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin(frontendUrl);
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
