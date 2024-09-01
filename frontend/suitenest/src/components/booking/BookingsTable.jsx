2290a54879527dd15999507d19ed81e4ad0f02f9
Sunny Goswami <sungoswami07gmail.com>
2024-09-01 12:50:19 +0530

    date format issue in admin manage booking - fixed

--- frontend/suitenest/src/components/booking/BookingsTable.jsx
+++ frontend/suitenest/src/components/booking/BookingsTable.jsx
@@ -45,35 +45,30 @@ const BookingsTable = ({ bookingsInfo, handleBookingCancellation }) => {
           </tr>
         </thead>
         <tbody className="text-center">
-          {filteredBookings.map((booking, index) => {
-            const formattedCheckInDate = format(parseISO(booking.checkInDate), "yyyy-MM-dd");
-            const formattedCheckOutDate = format(parseISO(booking.checkOutDate), "yyyy-MM-dd");
-
-            return (
-              <tr key={booking.id}>
-                <td>{index + 1}</td>
-                <td>{booking.id}</td>
-                <td>{booking.room.id}</td>
-                <td>{booking.room.roomType}</td>
-                <td>{formattedCheckInDate}</td>
-                <td>{formattedCheckOutDate}</td>
-                <td>{booking.guestFullName}</td>
-                <td>{booking.guestEmail}</td>
-                <td>{booking.numOfAdults}</td>
-                <td>{booking.numOfChildren}</td>
-                <td>{booking.totalNumOfGuest}</td>
-                <td>{booking.bookingConfirmationCode}</td>
-                <td>
-                  <button
-                    className="btn btn-danger btn-sm"
-                    onClick={() => handleBookingCancellation(booking.id)}
-                  >
-                    Cancel
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
+          {filteredBookings.map((booking, index) => (
+            <tr key={booking.id}>
+              <td>{index + 1}</td>
+              <td>{booking.id}</td>
+              <td>{booking.room.id}</td>
+              <td>{booking.room.roomType}</td>
+              <td>{format(parseISO(booking.checkInDate), "yyyy-MM-dd")}</td>
+              <td>{format(parseISO(booking.checkOutDate), "yyyy-MM-dd")}</td>
+              <td>{booking.guestFullName}</td>
+              <td>{booking.guestEmail}</td>
+              <td>{booking.numOfAdults}</td>
+              <td>{booking.numOfChildren}</td>
+              <td>{booking.totalNumOfGuest}</td>
+              <td>{booking.bookingConfirmationCode}</td>
+              <td>
+                <button
+                  className="btn btn-danger btn-sm"
+                  onClick={() => handleBookingCancellation(booking.id)}
+                >
+                  Cancel
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
       {filteredBookings.length === 0 && <p>No booking found for selected dates!</p>}
